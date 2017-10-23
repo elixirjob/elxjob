95b77bbff03706ad7e7b155ad01b6344d7bc9bf4
buurz <buurzx@gmail.com>
2017-10-23 19:52:35 +0300

    Update readme

--- lib/elxjob_web/controllers/page_controller.ex
+++ lib/elxjob_web/controllers/page_controller.ex
@@ -1,10 +1,6 @@
 defmodule ElxjobWeb.PageController do
   use ElxjobWeb, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-
   def project(conn, _params) do
     render conn, "project.html"
   end
