9ec0a2388ef2d4f5e6469669c9447f9a7d4da6cc
Pavel Ro <buurzx@gmail.com>
2017-12-02 14:30:45 +0300

    up to 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elxjob.Mixfile do
   def project do
     [
       app: :elxjob,
-      version: "0.0.1",
+      version: "0.0.3",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
