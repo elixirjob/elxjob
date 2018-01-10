c47457b2aa8c025304f15c9fa954db15545f67ec
Pavel Ro <buurzx@gmail.com>
2018-01-10 23:59:34 +0300

    up version

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,6 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
-import "contacts"
 
 // Import local files
 //
@@ -20,3 +19,40 @@ import "contacts"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+
+let showEmailBtn = document.getElementById("show-email");
+
+if (showEmailBtn) {
+    showEmailBtn.addEventListener("click", function(ev) {
+        ev.preventDefault();
+
+        let jobDiv = document.getElementById("job_email");
+        let jobID  = jobDiv.getAttribute("data-job-id");
+
+        getAjax(`/api/jobs/${jobID}/job_email`, function(data){ showEmail(data, jobDiv); });
+
+        showEmailBtn.style.display = "none"
+
+    })
+
+}
+
+
+function getAjax(url, success) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+    return xhr;
+}
+
+
+function showEmail(email, div) {
+    email = email.replace(/['"]+/g, '')
+    div.innerHTML +=
+        '<i class="mail icon"></i>' + `<a href="mailto:${email}">${email}</a>`
+}
